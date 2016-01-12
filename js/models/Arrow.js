bd30da6a306b235e55c5131f34431616ea31e65b
nmss <nmss@users.noreply.github.com>
2016-01-12 15:25:43 +0100

    Remove unused code

--- js/models/Arrow.js
+++ js/models/Arrow.js
@@ -7,39 +7,9 @@
 
   function Arrow(x, y, w, h, image) {
     this.updatePosition(x, y, w, h);
-    this.verts = [];
-    this.pVerts = [];
-
-    if (!image) {
-      return this.createBody();
-    }
-
     this.image = new Image();
-    this.image.src = config.arrow.image;
+    this.image.src = image;
   }
-
-  Arrow.prototype.createBody = function() {
-    this.createArrowShape();
-  };
-
-  Arrow.prototype.createArrowShape = function() {
-    var ppm = config.physics.ppm;
-
-    this.verts[0] = [0, this.h * 0.5];
-    this.verts[1] = [-this.w * 0.75, 0];
-    this.verts[2] = [0, -this.h * 0.5];
-    this.verts[3] = [this.w * 0.25, 0];
-
-    this.pVerts[0] = [this.verts[0][0] * ppm, -this.verts[0][1] * ppm];
-    this.pVerts[1] = [this.verts[1][0] * ppm, -this.verts[1][1] * ppm];
-    this.pVerts[2] = [this.verts[2][0] * ppm, -this.verts[2][1] * ppm];
-    this.pVerts[3] = [this.verts[3][0] * ppm, -this.verts[3][1] * ppm];
-
-    var shape = new p2.Convex(this.verts);
-    //shape.material = arrowMaterial;
-
-    return shape;
-  };
 
   Arrow.prototype.updatePosition = function(x, y, w, h) {
     this.x = x;
@@ -53,22 +23,7 @@
 
   Arrow.prototype.draw = function() {
     ctx.save();
-
-    if (!this.image) {
-      ctx.translate(this.pX, this.pY);
-      ctx.fillStyle = 'red';
-
-      ctx.beginPath();
-      ctx.moveTo(this.pVerts[0][0], this.pVerts[0][1]);
-      ctx.lineTo(this.pVerts[1][0], this.pVerts[1][1]);
-      ctx.lineTo(this.pVerts[2][0], this.pVerts[2][1]);
-      ctx.lineTo(this.pVerts[3][0], this.pVerts[3][1]);
-      ctx.closePath();
-      ctx.fill();
-    } else {
-      ctx.drawImage(this.image, this.pX - this.image.width * 0.65, this.pY - this.image.height * 0.5);
-    }
-
+    ctx.drawImage(this.image, this.pX - this.image.width * 0.65, this.pY - this.image.height * 0.5);
     ctx.restore();
   };
 
